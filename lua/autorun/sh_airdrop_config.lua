11cefe87c41374bdade6ae0480465ecccf3edca9
Ben <benpotten@gmail.com>
2018-03-24 13:58:09 +0000

    Update sh_airdrop_config.lua

--- lua/autorun/sh_airdrop_config.lua
+++ lua/autorun/sh_airdrop_config.lua
@@ -33,35 +33,16 @@ ryl_airdrop_config = {}
 ]]--
 
 ryl_airdrop_config.qualities = {
-	["common"] = {
+	["example_normal"] = {
 		spawnChance = 44,
-		printName   = "Common",
+		printName   = "example normal",
 		itemColour  = Color( 150, 150, 150 )
 	},
-	["uncommon"] = {
-		spawnChance = 30,
-		printName   = "Uncommon",
-		itemColour  = Color( 100, 255, 100 )
-	},
-	["unusual"] = {
-		spawnChance = 15,
-		printName   = "Unusual",
-		itemColour  = Color( 100, 100, 255 )
-	},
-	["rare"] = {
-		spawnChance = 7,
-		printName   = "Rare",
-		itemColour  = Color( 128, 0, 128 )
-	},
-	["ultrarare"] = {
-		spawnChance = 3,
-		printName   = "Ultra Rare",
-		itemColour  = Color( 255, 100, 100 )
-    },
-    ["premium"] = {
-    	spawnChance = 0.2,
-    	printName   = "PREMIUM",
-    	itemColour  = Color( 255, 153, 0 )
+	["example_vip"] = {
+		spawnChance = 70,
+		printName   = "example vip",
+		itemColour  = Color( 240, 20, 20 ),
+		reqRank     = { "vip1", "vip2" }
 	}
 }
 
@@ -80,75 +61,20 @@ ryl_airdrop_config.qualities = {
 ]]--
 
 ryl_airdrop_config.items = {
-	["airdrop_grenade"] = {
-		printName   = "Aidrop grenade",
-		itemEnt     = "weapons_airdrop_smokegrenade",
-		itemQuality = "premium"
-	},
-	["test1"] = {
-		printName   = "test1",
+	["example_normal_ball"] = {
+		printName   = "example 1",
 		itemEnt     = "sent_ball",
-		itemQuality = "premium",
+		itemQuality = "example_normal",
 	},
-	["test2"] = {
-		printName   = "test2",
+	["example_big_ball"] = {
+		printName   = "example 2",
 		itemEnt     = "sent_ball",
-		itemQuality = "premium" 
-	},
-	["test3"] = {
-		printName   = "test3",
-		itemEnt     = "sent_ball",
-		itemQuality = "ultrarare"
-	},
-	["test4"] = {
-		printName   = "test4",
-		itemEnt     = "sent_ball",
-		itemQuality = "ultrarare"
-	},
-	["test5"] = {
-		printName   = "test5",
-		itemEnt     = "sent_ball",
-		itemQuality = "rare"
-	},
-	["test6"] = {
-		printName   = "test6",
-		itemEnt     = "sent_ball",
-		itemQuality = "rare"
-	},
-	["test7"] = {
-		printName   = "test7",
-		itemEnt     = "sent_ball",
-		itemQuality = "unusual"
-	},
-	["test8"] = {
-		printName   = "test8",
-		itemEnt     = "sent_ball",
-		itemQuality = "unusual"
-	},
-	["test9"] = {
-		printName   = "test9",
-		itemEnt     = "sent_ball",
-		itemQuality = "uncommon"
-	},
-	["test10"] = {
-		printName   = "test10",
-		itemEnt     = "sent_ball",
-		itemQuality = "uncommon"
-	},
-	["test11"] = {
-		printName   = "test11",
-		itemEnt     = "sent_ball",
-		itemQuality = "common",
+		itemQuality = "example_normal",
 		itemFunc   = function( ply, ent ) ent:SetBallSize( 69 ) end
 	},
-	["test12"] = {
-		printName   = "test12",
-		itemEnt     = "sent_ball",
-		itemQuality = "common"
-	},
-	["plyhealth"] = {
-		printName   = "1000HP",
-		itemQuality = "common",
+	["example_player_health"] = {
+		printName   = "example 3",
+		itemQuality = "example_normal",
 		itemFunc    = function( ply, ent ) ply:SetHealth( ply:Health() + 1000 ) end
 	}
 }
