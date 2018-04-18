21c1e4cb85aa4c7d31de7464560305f64d1cee59
Ben <benpotten@gmail.com>
2018-04-18 23:10:43 +0100

    smoke on sleep

--- lua/entities/ryl_airdrop_grenade/shared.lua
+++ lua/entities/ryl_airdrop_grenade/shared.lua
@@ -4,3 +4,7 @@ ENT.PrintName      = "airdrop grenade entity"
 ENT.Category       = "ryl's airdrops" 
 ENT.Author         = "ryl"
 ENT.Spawnable      = false
+
+function ENT:SetupDataTables()
+	self:NetworkVar( "Bool", 0, "isSleeping" )
+end
