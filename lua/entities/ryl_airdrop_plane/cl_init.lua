9c2d203fc877820137eaa2ceaf8eb8b3aea4189e
Ben <benpotten@gmail.com>
2018-12-22 22:49:42 +0000

    Update cl_init.lua

--- lua/entities/ryl_airdrop_plane/cl_init.lua
+++ lua/entities/ryl_airdrop_plane/cl_init.lua
@@ -14,7 +14,7 @@ end
 
 function ENT:Think()
 	if IsValid( self.audioSource ) then
-		local newVol = ( self:GetPos() - LocalPlayer():GetPos() ):Length() / 25000
+		local newVol = 1 / ( 1 + math.exp(-( self:GetPos() - LocalPlayer():GetPos() ):Length()/25000))
 		self.audioSource:SetVolume( math.Clamp( 0.8 - newVol, 0 , 1 ) )
 
 		-- Weird bug where sound glitches out upon creation so don't enable it at creation
