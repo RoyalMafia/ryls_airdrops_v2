3943a73b2d0cb23bdaadbd32a94068098ba0c8f4
Ben <benpotten@gmail.com>
2018-12-22 22:47:48 +0000

    fix plane spawns

--- lua/autorun/server/sv_main.lua
+++ lua/autorun/server/sv_main.lua
@@ -45,6 +45,24 @@ sqlInit()
 
 ]]--
 
+function getPlaneStartPos( posToDrop )
+	local startPos = Vector( 0, 0, 0 )
+	local flightDir = Angle( 0, 0, 0 )
+
+	-- Get the boundary of the map then calculate the middle
+	local mapBoundsMin, mapBoundsMax = game.GetWorld():GetModelBounds()
+	local mapCenter = Vector( math.sqrt( mapBoundsMax.x - mapBoundsMin.x )*2, math.sqrt( mapBoundsMax.y - mapBoundsMin.y )*2, 0 )
+
+	-- Get a random direction then find the position on a circle inside the map boundaries
+	local randDirection = math.Rand( 0, 360 )
+	local startPos = mapCenter + Vector( math.cos( randDirection ) * math.abs( mapBoundsMin.x - mapBoundsMax.x )/2, math.sin( randDirection ) * math.abs( mapBoundsMin.x - mapBoundsMax.x )/2, math.Clamp( mapBoundsMax.z/1.2, 0, 4000 ) )
+	
+	-- Calculate the angle between the spawn position and drop position so plane flies towards the drop
+	local flightDir = ( Vector( posToDrop.x, posToDrop.y, 0 ) - Vector( startPos.x, startPos.y, 0 ) ):Angle()
+
+	return startPos, flightDir
+end
+
 local function checkPlyRank( ply, ranktable )
 	for k, v in pairs( ranktable ) do
 		if ply:GetUserGroup() == v then
@@ -57,17 +75,9 @@ local function spawnDrop()
 	if #ryl_airdrop_positions > 0 then
 		-- Select a position from the list
 		local posToDrop = ( #ryl_airdrop_positions > 1 and ryl_airdrop_positions[math.random( 1, #ryl_airdrop_positions )] or ryl_airdrop_positions[1] )
-		
-		-- Get the boundary of the map then calculate the middle
-		local mapBoundsMin, mapBoundsMax = game.GetWorld():GetModelBounds()
-		local mapCenter = Vector( math.sqrt( mapBoundsMax.x - mapBoundsMin.x )*2, math.sqrt( mapBoundsMax.y - mapBoundsMin.y )*2, 0 )
-		
-		-- Get a random direction then find the position on a circle inside the map boundaries
-		local randDirection = math.Rand( 0, 360 )
-		local flightStartPos = mapCenter + Vector( math.cos( randDirection ) * math.abs( mapBoundsMin.x - mapBoundsMax.x )/2, math.sin( randDirection ) * math.abs( mapBoundsMin.x - mapBoundsMax.x )/2, 500 )
-		
-		-- Calculate the angle between the spawn position and drop position so plane flies towards the drop
-		local flightDirection = ( Vector( posToDrop.x, posToDrop.y, 0 ) - Vector( flightStartPos.x, flightStartPos.y, 0 ) ):Angle()
+
+
+		local flightStartPos, flightDirection = getPlaneStartPos( posToDrop )
 
 		-- Spawn the plane in and set its drop position
 		local planeEnt = ents.Create( "ryl_airdrop_plane" )
